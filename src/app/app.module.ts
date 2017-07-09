63dcdf48112a6f9133c40fc5f8ecbf594d563d1e
Hello Tunmbi <hellotunmbi@gmail.com>
2017-07-09 13:02:15 +0100

    new voice and login component plus modifications in other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './modules/header/header.component';
 import { SidebarComponent } from './modules/sidebar/sidebar.component';
 import { HomeComponent } from './views/home/home.component';
 import { ComplaintsComponent } from './modules/complaints/complaints.component';
+import { VoiceComponent } from './views/voice/voice.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { ComplaintsComponent } from './modules/complaints/complaints.component';
     HeaderComponent,
     SidebarComponent,
     HomeComponent,
-    ComplaintsComponent
+    ComplaintsComponent,
+    VoiceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,22 @@
 import { Routes, RouterModule } from "@angular/router";
 
 import { HomeComponent } from "./views/home/home.component";
+import { VoiceComponent } from "./views/voice/voice.component";
+import { LoginComponent } from "./views/login/login.component";
+
 
 const appRoutes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'voice',
+        component: VoiceComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
 
