9639ec922f8c6be44e004367da3885cd616b4079
Hello Tunmbi <hellotunmbi@gmail.com>
2017-07-24 11:13:31 +0100

    adding services to complaints component

--- src/app/modules/complaints/complaints.component.ts
+++ src/app/modules/complaints/complaints.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ComplaintsService } from "../../services/complaints.service";
+import { RantComponent } from "../rant/rant.component";
+
 @Component({
   selector: 'app-complaints',
-  templateUrl: './complaints.component.html',
+ templateUrl: './complaints.component.html',
   styleUrls: ['./complaints.component.css']
 })
 export class ComplaintsComponent implements OnInit {
 
-  constructor() { }
+  complaints: any[];
+
+  constructor(
+    private complaintsService: ComplaintsService
+  ) { }
+
+    
+  getComplaints() {
+    this.complaintsService.getAllComplaints().then((complaints: any[]) => this.complaints = complaints);
+  }
+
+  // loadUser() {
+  //   this.complaintsService.getUser().subscribe(data => this.complaints = data);
+  // }
 
   ngOnInit() {
+    this.complaints = [];
+    console.log(this.getComplaints());
+
+
+
   }
 
 }
